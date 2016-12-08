754e6652177c679e57bc6c19760aafabfd903d21
Wilson Ng <Wilson@Wilsons-MacBook-Pro.local>
2016-12-07 16:51:33 -0800

    Convert server to use html files

--- trippin/app.js
+++ trippin/app.js
@@ -4,15 +4,25 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var serveStatic = require('serve-static');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var error = require('./routes/error');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+app.use(serveStatic(path.join(__dirname, 'views'), {
+  'index': 'index.html'
+}));
+app.use('/error404', serveStatic(path.join(__dirname, 'views'), {
+  'index': 'error404.html'
+}));
+app.use('/error', serveStatic(path.join(__dirname, 'views'), {
+  'index': 'error.html'
+}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +33,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +49,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  if (err.status === 404) res.redirect('/error404');
+  else res.redirect('/error');
 });
 
 module.exports = app;
