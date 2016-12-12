5f9f0b428bd9243c5f0340066ce7681fc7d5530e
IceFractal <jdenea@outlook.com>
2016-12-12 12:03:07 -0800

    Modularize content string generation

--- trippin/public/gservice/convertToMapPoints.js
+++ trippin/public/gservice/convertToMapPoints.js
@@ -10,36 +10,7 @@ window.convertToMapPoints = function(response) {
     var pin = response[i];
 
     // Create popup windows for each record
-    var contentString =
-        '<p><span class="pin-title">' + pin.title +
-        '</span><br>' + pin.description + '</p><div id="' + pin._id + '" />';
-    
-    
-    var display    = document.createElement('img');
-    display.src    = pin.image;
-    display.style  = 'visibility:hidden;';
-        
-    var img = new Image();
-    img.onload = (function(id, display) {
-      return function(e) {
-        google.maps.event.addListener(savedInfoWindow, 'domready', function() {
-          var element = document.getElementById(id);
-          if (last === id) {
-            display.style = 'visibility:hidden;';
-          }
-          if (element) {
-            display.height = ((e.path[0].height / e.path[0].width) * 140);
-            display.width  = ((e.path[0].width / e.path[0].height) * 140);
-            element.appendChild(display);
-            setTimeout(function() {
-              last = id;
-              display.style = '';
-            }, 100);
-          }
-        });
-      };
-    })(pin._id, display);
-    img.src = pin.image;
+    var contentString = contentStringGen(pin);
     
     // Converts each of the JSON records into Google Maps Location format (Note [Lat, Lng] format).
     locations.push({
--- trippin/public/gservice/saveData.js
+++ trippin/public/gservice/saveData.js
@@ -26,36 +26,7 @@ window.saveData = function($sanitize, $http) {
     }).then(function successCallback(response) {
 
       // Create a popup window for the new location
-      var contentString =
-        '<p><span class="pin-title">' + pin.title +
-        '</span><br>' + pin.description + '</p><div id="' + pin._id + '" />';
-    
-    
-      var display    = document.createElement('img');
-      display.src    = pin.image;
-      display.style  = 'visibility:hidden;';
-
-      var img = new Image();
-      img.onload = (function(id, display) {
-        return function(e) {
-          google.maps.event.addListener(savedInfoWindow, 'domready', function() {
-            var element = document.getElementById(id);
-            if (last === id) {
-              display.style = 'visibility:hidden;';
-            }
-            if (element) {
-              display.height = ((e.path[0].height / e.path[0].width) * 140);
-              display.width  = ((e.path[0].width / e.path[0].height) * 140);
-              element.appendChild(display);
-              setTimeout(function() {
-                last = id;
-                display.style = '';
-              }, 100);
-            }
-          });
-        };
-      })(pin._id, display);
-      img.src = pin.image;
+      var contentString = contentStringGen(pin);
 
       // define the new location
       var newLoc = {
