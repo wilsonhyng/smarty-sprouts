0c3b6bf6d24d64bba19ed9b9fc518ccdee76ee9d
Yoshi Oritatsu <yoshioritatsu@gmail.com>
2016-12-12 20:06:02 -0800

    Added Test folder, required more npm modules, and added grunt files for testing.

---
+++ trippin/Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**.js']
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.registerTask('server-dev', function (target) {
+    grunt.task.run([ 'nodemon', 'watch' ]);
+  });
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('test', [
+    'mochaTest'
+  ]);
+};
